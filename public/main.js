a16701999f90963bf129d2e69764693d53689361
Luke <luke@Lukes-MacBook-Pro.local>
2018-08-17 09:47:53 -0700

    created moving dog gif

--- public/main.js
+++ public/main.js
@@ -3,9 +3,4 @@ $('.carousel').carousel({
 });
 
 
-const api_key = "1288fc9154ba4d0fbd7f07db4269e04b"
 
-$.ajax({
-    method: 'GET',
-    url: http://open.spotify.com/track/6rqhFgbbKwnb9MLmUQDhG6'
-})
