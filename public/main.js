016f2fb09ec5681bdec0fa75289e819ad38be968
Luke <luke@Lukes-MacBook-Pro.local>
2018-08-16 19:06:00 -0700

    created carousel gallery and moved nav bar

---
+++ public/main.js
@@ -0,0 +1,11 @@
+$('.carousel').carousel({
+    interval: 2000
+});
+
+
+const api_key = "1288fc9154ba4d0fbd7f07db4269e04b"
+
+$.ajax({
+    method: 'GET',
+    url: http://open.spotify.com/track/6rqhFgbbKwnb9MLmUQDhG6'
+})
